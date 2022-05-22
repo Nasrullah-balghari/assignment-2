e86646c918783ed6150b4fe74f6f2dd6f57aaec5
Nasrullah-balghari <nasrullahbalghari676@gmail.com>
2022-05-22 13:46:54 +0500

    assignment-2-from-chapter-2

---
+++ app.js
@@ -0,0 +1,18 @@
+
+//Question-1
+// var age = 24; 
+// alert("I am " +age+ " years old");
+
+//Quetion- 2
+// var visit = "you have visited this site N times";
+// alert(visit);
+
+//Question-3
+// var birthYear = 1998;
+// document.write("My birth year is "+birthYear+   " <br> Datatype of my declared variable is number");
+
+//Question-4
+// var name ="Nasrullah";
+// var product = "dress pent";
+// var quantity = 4;
+// document.write(name + " ordered " +quantity+ " "+ product+"(s) on XYZ Clothing store");
